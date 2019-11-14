7644cdc99292d7163c528abf9def6ad4f9d1681d
Amanda Loudon <amandagloudon@Amandas-MacBook-Pro-2.local>
2019-11-14 13:53:29 -0600

    letter function

---
+++ letter func.R
@@ -0,0 +1,5 @@
+alphabet <- c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+
+letter <- function(x){
+  return(sample(alphabet,size = 1))
+}
