ffb1784419b8219d008f2f122eb68087a67ddd84
Amanda Loudon <amandagloudon@Amandas-MacBook-Pro-2.local>
2019-11-14 14:06:28 -0600

    alphabet soup

---
+++ AlphabetSoup/R/letter.R
@@ -0,0 +1,5 @@
+letter <-
+function(x){
+  alphabet <- c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+  return(sample(alphabet,size = 1))
+}
